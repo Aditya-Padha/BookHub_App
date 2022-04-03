fd38ca840844eb4fccca2585ebe89f6547f84b4e
Aditya Padha <sharluk222@gmail.com>
2022-04-03 17:40:02 +0530

    changes

---
+++ app/src/main/java/com/aditya/bookhub/activity/DescriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.aditya.bookhub.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aditya.bookhub.R
+
+class DescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_description)
+    }
+}
